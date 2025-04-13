1c2e3bd770f29588f025a6968d66b038973735e7
Kylan <kylan2837@gmail.com>
2025-04-13 16:57:51 +0200

    It's starting to be playable

--- scripts/player/player.gd
+++ scripts/player/player.gd
@@ -1,14 +1,21 @@
-extends Area2D
+extends RigidBody2D
 
 @export var speed = 400 # How far the player will move pixel/sec.
+@export var jump = 500 # How far the player will jump.
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+# Called every frame. 'delta' is the elapsed time since t0he previous frame.
+func _process(_delta: float) -> void:
+	if(Input.is_action_just_released("action_jump")):
+		apply_central_impulse(Vector2.UP * jump)
+	pass
+	
+# Called every physic update.
+func _integrate_forces(_state: PhysicsDirectBodyState2D) -> void:
 	var velocity = Vector2.ZERO
 	if(Input.is_action_pressed("move_left")):
 		velocity.x -= 1
@@ -20,4 +27,4 @@ func _process(delta: float) -> void:
 		$AnimatedSprite2D.flip_h = velocity.x > 0
 	else:
 		$AnimatedSprite2D.play("idle")
-	position += velocity * delta
+	linear_velocity.x = velocity.x
